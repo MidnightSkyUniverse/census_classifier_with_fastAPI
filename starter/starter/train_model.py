e582756d8fed49c2cb20e88e12ac5a816577e33d
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-14 18:25:45 +0100

    update to fit and predict

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -7,6 +7,7 @@ import logging
 from numpy import array
 import pandas as pd
 from sklearn.model_selection import KFold
+import joblib
 
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
@@ -19,7 +20,7 @@ logger = logging.getLogger()
 def go(config: DictConfig):
 
     # Get the cleaned data
-    logger.info('importing data from {config['data']['cleaned_data']}')
+    logger.info(f"importing data from {config['data']['cleaned_data']}")
     data = pd.read_csv(config['data']['cleaned_data'])
 
     # prepare cross validation
@@ -43,15 +44,29 @@ def go(config: DictConfig):
         "native-country",
     ]
     logger.info('Split the data into X_train and y_train')
-    
-    X_train, y_train, encoder, lb = process_data(
+    # Separate X and y
+    X, y, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
+    # enumerate through splits
+    scores = list()
+    logger.info('Enumerating through k-folds')
+    for train_x_split, train_y_split in kfold.split(data):
+        X_train, X_test = X[train_x_split], X[test_x_split]
+        y_train, y_test = y[train_x_split], y[test_x_split]
+        
+        # train model    
+        logger.info('train the model')
+        model = train_model(X_train, y_train) 
+       
+        # inference
+        logger.info('inference')
+        preds = inference(model, X_test) 
 
-
-# Train and save a model.
-
-
+        precision, recall, fbeta = compute_model_metrics(y_test, preds)
+        logger.info('precision: {precision}')
+        logger.info('recall: {recall}')
+        logger.info('fbeta: {fbeta}')
 
 
 if __name__ == "__main__":
