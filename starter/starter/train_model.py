1e0d351016f498099d92f9aa82d44ca440cf8187
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-14 20:06:13 +0100

    Workint train/predict model for 10 kfold

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,14 @@
 # Script to train machine learning model.
 
-#from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 import hydra
 import logging
+from omegaconf import DictConfig
 
-from numpy import array
+# from numpy import array
 import pandas as pd
 from sklearn.model_selection import KFold
-import joblib
+# import joblib
 
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
@@ -16,22 +17,19 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-@hydra.main(config_name='config')
-def go(config: DictConfig):
+@hydra.main(config_path='.', config_name='config')
+def go(cfg: DictConfig):
 
     # Get the cleaned data
-    logger.info(f"importing data from {config['data']['cleaned_data']}")
-    data = pd.read_csv(config['data']['cleaned_data'])
+    logger.info(f"importing data from {cfg.data.cleaned_data}")
+    pth = f"{hydra.utils.get_original_cwd()}/{cfg.data.dir}{cfg.data.cleaned_data}"
+    data = pd.read_csv(pth)
 
     # prepare cross validation
     logger.info('Prepare cross validation with Kfold')
-    kfold = KFold(config['modeling']['n_splits'], \
-                config['modeling']['shuffle'], \
-                config['modeling']['random_state'])
-
-# enumerate splits
-#for train, test in kfold.split(data):
-#	print('train: %s, test: %s' % (data[train], data[test]))
+    kfold = KFold(n_splits=cfg['modeling']['n_splits'],\
+                  shuffle=cfg['modeling']['shuffle'],\
+                  random_state=cfg['modeling']['random_state'])
 
     cat_features = [
         "workclass",
@@ -43,32 +41,32 @@ def go(config: DictConfig):
         "sex",
         "native-country",
     ]
+
     logger.info('Split the data into X_train and y_train')
     # Separate X and y
     X, y, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
+        data, categorical_features=cat_features, label="salary", training=True
     )
     # enumerate through splits
-    scores = list()
+    # scores = list()
     logger.info('Enumerating through k-folds')
-    for train_x_split, train_y_split in kfold.split(data):
+    for train_x_split, test_x_split in kfold.split(data):
         X_train, X_test = X[train_x_split], X[test_x_split]
         y_train, y_test = y[train_x_split], y[test_x_split]
-        
-        # train model    
+
+        # train model
         logger.info('train the model')
-        model = train_model(X_train, y_train) 
-       
+        model = train_model(X_train, y_train)
+
         # inference
         logger.info('inference')
-        preds = inference(model, X_test) 
+        preds = inference(model, X_test)
 
         precision, recall, fbeta = compute_model_metrics(y_test, preds)
-        logger.info('precision: {precision}')
-        logger.info('recall: {recall}')
-        logger.info('fbeta: {fbeta}')
+        logger.info(f'precision: {precision}')
+        logger.info(f'recall: {recall}')
+        logger.info(f'fbeta: {fbeta}')
 
 
 if __name__ == "__main__":
     go()
-
