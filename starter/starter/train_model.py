a735e8caa8073e13e236cf211dc7b0a7d8deab21
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-14 17:59:32 +0100

    KFold defined, imports updated, config.yaml for hydra created

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,31 +1,59 @@
 # Script to train machine learning model.
 
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
+import hydra
+import logging
+
+from numpy import array
 import pandas as pd
+from sklearn.model_selection import KFold
 
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
 
-# Get the cleaned data
-data = pd.read_csv('../data/cleaned_data.csv')
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+@hydra.main(config_name='config')
+def go(config: DictConfig):
 
-# Proces the test data with the process_data function.
+    # Get the cleaned data
+    logger.info('importing data from {config['data']['cleaned_data']}')
+    data = pd.read_csv(config['data']['cleaned_data'])
+
+    # prepare cross validation
+    logger.info('Prepare cross validation with Kfold')
+    kfold = KFold(config['modeling']['n_splits'], \
+                config['modeling']['shuffle'], \
+                config['modeling']['random_state'])
+
+# enumerate splits
+#for train, test in kfold.split(data):
+#	print('train: %s, test: %s' % (data[train], data[test]))
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    logger.info('Split the data into X_train and y_train')
+    
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+
 
 # Train and save a model.
+
+
+
+
+if __name__ == "__main__":
+    go()
+
