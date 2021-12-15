60bee3964c7236bb4a1583746170f2c2ca9c056f
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-15 11:22:20 +0100

    added two more models and ROC ruvce chart

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,8 +1,16 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_roc_curve
+
+import joblib
 
 
-# Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
+
+
+def train_KNeighbours_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
 
@@ -18,10 +26,58 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    model = KNeighborsClassifier()
+    model.fit(X_train, y_train)
+    
+    return model
+
+def train_LogisticRegression_model(X_train, y_train,random_state,max_iter):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    model = LogisticRegression(random_state=random_state, max_iter=max_iter)
+    model.fit(X_train, y_train)
+    
+    return model
 
 
-def compute_model_metrics(y, preds):
+def train_RandomForest_model(X_train, y_train,random_state):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    model = RandomForestClassifier(random_state=random_state)
+    model.fit(X_train, y_train)
+    
+    return model
+
+
+
+
+def compute_model_metrics(y_test, preds):
     """
     Validates the trained machine learning model using precision, recall, and F1.
 
@@ -37,13 +93,13 @@ def compute_model_metrics(y, preds):
     recall : float
     fbeta : float
     """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
-    precision = precision_score(y, preds, zero_division=1)
-    recall = recall_score(y, preds, zero_division=1)
+    fbeta = fbeta_score(y_test, preds, beta=1, zero_division=1)
+    precision = precision_score(y_test, preds, zero_division=1)
+    recall = recall_score(y_test, preds, zero_division=1)
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model, X_test):
     """ Run model inferences and return the predictions.
 
     Inputs
@@ -57,4 +113,22 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X_test)
+    return preds
+
+def roc_curve_plot(model_1, model_2, model_3,X_test,y_test,pth):
+    '''
+        creates and stores the feature importances in pth
+        input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+        output:
+             None
+    '''
+    ax = plt.gca()
+    fig = plot_roc_curve(model_1, X_test, y_test, ax=ax, alpha=0.8)
+    fig = plot_roc_curve(model_2, X_test, y_test, ax=ax, alpha=0.8)
+    fig = plot_roc_curve(model_3, X_test, y_test, ax=ax, alpha=0.8)
+    plt.savefig(pth, bbox_inches='tight')
+    plt.clf()
+    #lrc_plot = plot_roc_curve(model_2, X_test, y_test, ax=ax, alpha=0.8)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -11,7 +11,8 @@ from sklearn.model_selection import KFold
 # import joblib
 
 from ml.data import process_data
-from ml.model import train_model, compute_model_metrics, inference
+from ml.model import train_KNeighbours_model, train_LogisticRegression_model, \
+        train_RandomForest_model, compute_model_metrics, inference, roc_curve_plot
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -27,8 +28,8 @@ def go(cfg: DictConfig):
 
     # prepare cross validation
     logger.info('Prepare cross validation with Kfold')
-    kfold = KFold(n_splits=cfg['modeling']['n_splits'],\
-                  shuffle=cfg['modeling']['shuffle'],\
+    kfold = KFold(n_splits=cfg['modeling']['n_splits'],
+                  shuffle=cfg['modeling']['shuffle'],
                   random_state=cfg['modeling']['random_state'])
 
     cat_features = [
@@ -48,7 +49,9 @@ def go(cfg: DictConfig):
         data, categorical_features=cat_features, label="salary", training=True
     )
     # enumerate through splits
-    # scores = list()
+    metrics_1 = list()
+    metrics_2 = list() 
+    metrics_3 = list()
     logger.info('Enumerating through k-folds')
     for train_x_split, test_x_split in kfold.split(data):
         X_train, X_test = X[train_x_split], X[test_x_split]
@@ -56,16 +59,66 @@ def go(cfg: DictConfig):
 
         # train model
         logger.info('train the model')
-        model = train_model(X_train, y_train)
+        model_1 = train_KNeighbours_model(X_train, y_train)
+        model_2 = train_LogisticRegression_model(X_train, y_train, cfg.modeling.random_state, cfg.modeling.max_iter)
+        model_3 = train_RandomForest_model(X_train, y_train,cfg.modeling.random_state)
 
         # inference
         logger.info('inference')
-        preds = inference(model, X_test)
+        preds_1 = inference(model_1, X_test)
+        preds_2 = inference(model_2, X_test)
+        preds_3 = inference(model_3, X_test)
 
-        precision, recall, fbeta = compute_model_metrics(y_test, preds)
-        logger.info(f'precision: {precision}')
-        logger.info(f'recall: {recall}')
-        logger.info(f'fbeta: {fbeta}')
+        precision, recall, fbeta = compute_model_metrics(y_test, preds_1)
+        metrics_1.append((model_1, precision, recall, fbeta))
+
+        precision, recall, fbeta = compute_model_metrics(y_test, preds_2)
+        metrics_2.append((model_2, precision, recall, fbeta))
+
+        precision, recall, fbeta = compute_model_metrics(y_test, preds_3)
+        metrics_3.append((model_3, precision, recall, fbeta))
+
+
+    # Select model with highest precision and save it
+    precision_ = 0
+    for model, precision, recall, fbeta in metrics_1:
+    #    print(f".....precision: {precision}, recall: {recall}, fbeta: {fbeta}")
+
+        if precision > precision_:
+            model_1 = model
+            precision_ = precision
+            recall_ = recall
+            fbeta = fbeta
+
+    print(f"best model_1 precision: {precision}")
+
+    precision_ = 0
+    for model, precision, recall, fbeta in metrics_2:
+     #   print(f".....precision: {precision}, recall: {recall}, fbeta: {fbeta}")
+
+        if precision > precision_:
+            model_2 = model
+            precision_ = precision
+            recall_ = recall
+            fbeta = fbeta
+
+    print(f"best model_2 precision: {precision}")
+
+    precision_ = 0
+    for model, precision, recall, fbeta in metrics_3:
+      #  print(f".....precision: {precision}, recall: {recall}, fbeta: {fbeta}")
+
+        if precision > precision_:
+            model_3 = model
+            precision_ = precision
+            recall_ = recall
+            fbeta_ = fbeta
+
+    print(f"best model_3 precision: {precision}")
+
+    # Select best performing model and draw roc_curve
+    pth = f"{hydra.utils.get_original_cwd()}/{cfg.metrics.dir}{cfg.metrics.roc}" 
+    roc_curve_plot(model_1,model_2,model_3,X_test,y_test,pth)
 
 
 if __name__ == "__main__":
