c0fb6d715d0b5f22e862d1c74b976974bec337d4
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-21 21:27:50 +0100

    added second POST test

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -15,3 +15,8 @@ def test_predict(json_sample):
     assert response.status_code == 200
     assert response.json()['result'] == 1
 
+def test_predict_2(json_sample_2):
+    response = client.post("/predict",json=json_sample_2)
+    assert response.status_code == 200
+    assert response.json()['result'] == 0
+
