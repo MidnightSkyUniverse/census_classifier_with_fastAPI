57be7a07c3ad329bd40e10edbd42786e33b4b48f
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-28 08:43:34 +0100

    modify main for torubleshooting

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import uvicorn
 import logging
 import os
 
-from sklearn.ensemble import RandomForestClassifier
 from starter.ml.data import process_data
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -17,6 +16,7 @@ logger = logging.getLogger()
 
 # DVC on Heroku - required code
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    echo $AWS_ACCESS_KEY_ID
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
@@ -25,6 +25,8 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 app = FastAPI()
 
 # Home site with welcome message
+
+
 @app.get("/", tags=["home"])
 async def get_root() -> dict:
     return {
