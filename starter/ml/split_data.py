193ea9afe95e67cd878ebdcc12fad207b9b400b4
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-12-31 15:23:00 +0100

    updates of three steps

---
+++ starter/ml/split_data.py
@@ -0,0 +1,52 @@
+"""
+Author: Ali Binkowska
+Date: Dec 2021
+
+The 
+"""
+import logging
+import os
+import yaml 
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+logger = logging.getLogger()
+
+
+def go():
+
+    # Get the clean data
+    artifacts = yaml.safe_load(open("params.yaml"))["data"]
+    pwd = os.getcwd()
+    pth =  f"{pwd}/{artifacts['clean_data']}"
+    logger.info(f"Importe data from {artifacts['clean_data']}")
+    try:
+        data = pd.read_csv(pth)
+    except FileNotFoundError:
+        logger.error("Failed to load the file")
+
+    # Split the data into training+validation and test data for test
+    modeling = yaml.safe_load(open("params.yaml"))["modeling"]
+    logger.info(f"Split the data with test_size={modeling['test_size']}")
+    trainval, test = train_test_split(
+            data,
+            test_size = modeling['test_size'],
+            random_state=modeling['random_state']    
+    )
+
+    # Save trainval and test data as csv files
+    for df, k in zip([trainval, test], [artifacts['trainval_data'], artifacts['test_data']]):
+        logger.info(f"Saving {k} dataset")
+        output = f"{pwd}/{k}"
+        try:
+            df.to_csv(output, index=False)
+        except BaseException:
+            logger.error("Failed to save file {k}")
+
+
+
+if __name__ == '__main__':
+
+    go()
